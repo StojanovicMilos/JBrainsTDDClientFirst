60ac9c78fee584d084d9668c1ff91cb88640afea
Milos Stojanovic <milos-stojanovic@hotmail.com>
2017-12-06 19:52:32 +0100

    Add project files.

---
+++ BarcodeScannerClientFirstTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BarcodeScannerClientFirstTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BarcodeScannerClientFirstTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64070333-0093-4b83-8a76-f9dd08cd791b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BarcodeScannerClientFirstTests/SellOneItemControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BarcodeScannerClientFirstTests
+{
+    [TestClass]
+    public class SellOneItemControllerTests
+    {
+        [TestMethod]
+        public void ProductFound()
+        {
+            SaleController.OnBarcode("12345");
+
+            Assert.AreEqual(Display.DisplayPrice(Price.Cents(795)));
+        }
+    }
+}
